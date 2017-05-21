9ad0cc91475e64e6444a8b4e74f33711a8296996
Rafael Souza Fijalkowski <rafaelsouzaf@gmail.com>
2017-05-21 16:32:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,21 +31,16 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-##
-## EXECUTE THIS SCRIPT WITH ROOT
-##
-## SYSTEM REQUIREMENTS:
-##		root access
-##		pkg installed
+#
+# EXECUTE THIS SCRIPT WITH ROOT
+#
+# SYSTEM REQUIREMENTS:
+#		root access
+#		pkg installed
 
 
-
-##
-## GLOBAL VARS
-##
 set USERNAME = 'MY_USERNAME'
 set TRANSMISSION_PASSWORD = 'MY_PASSWORD'
-mkdir -p /Media/Downloads
 
 if ($#argv == 0) then
 	echo "FreeBSD After Install"
@@ -53,36 +48,24 @@ if ($#argv == 0) then
 	exit 1
 endif
 
+
+
 printf "\n\n\n\n"
 echo "########################################################"
 echo "################# UPDATING FREEBSD "
 echo "########################################################"
-
 freebsd-update fetch
 freebsd-update install
 
 
-
-##
-## INSTALL BASIC PROGRAMS
-##
 printf "\n\n\n\n"
 echo "########################################################"
 echo "################# INSTALLING BASIC PROGRAMS"
 echo "########################################################"
-pkg install -y vim nano git wget curl openjdk unzip
+pkg install -y vim nano git wget curl openjdk unzip gcc compat9x-amd64
 
 
 
-##
-## ALLOW REMOTE LOGIN BY SSH WITH ROOT USER
-##
-##		Edit file /etc/ssh/sshd_config
-##		Find this line:
-##		    #PermitRootLogin no
-##		and change it to:
-##		    PermitRootLogin yes
-##		Restart sshd
 printf "\n\n\n\n"
 echo "########################################################"
 echo "################# ALLOW REMOTE LOGIN BY SSH WITH ROOT USER"
@@ -101,6 +84,7 @@ pkg install -y sudo
 echo "$USERNAME ALL=(ALL) ALL" >> /usr/local/etc/sudoers
 
 
+
 printf "\n\n\n\n"
 echo "########################################################"
 echo "################# INSTALL VIRTUALBOX GUEST ADDICTIONS"
@@ -111,15 +95,12 @@ echo "########################################################"
 
 
 
-##
-## INSTALL SPEEDTEST
-##
 printf "\n\n\n\n"
 echo "########################################################"
 echo "################# INSTALL SPEED TEST"
 echo "########################################################"
-
 pkg install -y py27-speedtest-cli
+
 
 
 printf "\n\n\n\n"
@@ -127,7 +108,6 @@ echo "########################################################"
 echo "################# INSTALL TRANSMISSION"
 echo "################# http://localhost:9091"
 echo "########################################################"
-
 pkg install -y transmission transmission-cli transmission-daemon
 sysrc transmission_enable="YES"
 sysrc transmission_watch_dir="/Media/Downloads"
@@ -136,7 +116,6 @@ service transmission onestart
 service transmission onestop
 pw groupmod transmission -m rafaelsouzaf
 pw groupmod rafaelsouzaf -m transmission
-
 sed -i -- 's/\/usr\/local\/etc\/transmission\/home\/Downloads/\/Media\/Downloads/g' /usr/local/etc/transmission/home/settings.json
 sed -i -- 's/"rpc-username": ""/"rpc-username": "$USERNAME"/g' /usr/local/etc/transmission/home/settings.json
 sed -i -- 's/"rpc-whitelist": "127.0.0.1"/"rpc-whitelist": "0.0.0.0"/g' /usr/local/etc/transmission/home/settings.json
@@ -160,42 +139,46 @@ echo "########################################################"
 echo "################# INSTALL PLEX MEDIA SERVER"
 echo "################# http://localhost:32400/web/"
 echo "########################################################"
-
-pkg install -y gcc compat9x-amd64
+mkdir -p /Media/Downloads
 cd /Media
 wget https://downloads.plex.tv/plex-media-server/1.5.6.3790-4613ce077/PlexMediaServer-1.5.6.3790-4613ce077-freebsd-amd64.tar.bz2
 tar -vxjf PlexMediaServer-1.5.6.3790-4613ce077-freebsd-amd64.tar.bz2
 rm PlexMediaServer-1.5.6.3790-4613ce077-freebsd-amd64.tar.bz2
 
-##
-## A simple rc.d script to Plex starts automatically
-##
-#	#!/bin/sh
-#
-#	. /etc/rc.subr
-#
-#	name="plex"
-#	start_cmd="${name}_start"
-#	stop_cmd=":"
-#
-#	plex_start()
-#	{
-#        cd /Media/PlexMediaServer-1.5.6/ && ./start.sh
-#        echo "Plex started."
-#	}
-#
-#	load_rc_config $name
-#	run_rc_command "$1"
 
+
+printf "\n\n\n\n"
+echo "########################################################"
+echo "################# CREATING A SIMPLE RC.D SCRIPT TO PLEX"
+echo "################# STARTS AUTOMATICALLY"
+echo "########################################################"
+cat <<EOF > /etc/rc.d/plex
+#!/bin/sh
+. /etc/rc.subr
+name="plex"
+start_cmd="${name}_start"
+stop_cmd=":"
+plex_start() {
+	cd /root/PlexMediaServer-1.5.6/ && ./start.sh
+	echo "Plex started."
+}
+load_rc_config $name
+run_rc_command "$1"
+EOF
+
+#
+# add plex service in rc.conf
+#
+chmod +x /etc/rc.d/plex
 sysrc plex_enable="YES"
 
-##
-## SET PERMISSION FOLDER
-##
+#
+# SET PERMISSION FOLDER
+#
 chmod -R 755 /Media
-chown -R $USERNAME:$USERNAME /Media
+chown -R "$USERNAME:$USERNAME" /Media
 ln -s /Media /home/$USERNAME/Media
-chown -R $USERNAME:$USERNAME /home/$USERNAME/Media
+chown -R "$USERNAME:$USERNAME" /home/$USERNAME/Media
 
 
 
@@ -206,5 +189,3 @@ echo "########################################################"
 echo "################# PLEX: http://localhost:32400/web/"
 echo "################# TRANSMISSION: http://localhost:9091"
 echo "########################################################"
-
-
